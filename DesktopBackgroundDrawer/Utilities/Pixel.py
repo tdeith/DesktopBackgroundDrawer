9e985296a25203ee989d67db863074f27d61c266
tdeith <tdeith@gmail.com>
2014-03-07 14:06:36 -0500

    Initial commit of Pixel class. Mostly still figuring out how GIT works, though...

---
+++ DesktopBackgroundDrawer/Utilities/Pixel.py
@@ -0,0 +1,11 @@
+from Colours import Colour
+
+class Pixel(object):
+    '''
+    The pixel class is used to represent a single pixel, with accessors to the neighbours of that pixel, 
+    the pixel's colour, and pixel's location.  
+    '''
+
+    def __init__(self, x = 0, y = 0, colour = Colour(), neighbours=[]):
+        
+                
